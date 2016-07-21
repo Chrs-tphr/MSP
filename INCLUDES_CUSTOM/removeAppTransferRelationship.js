ce8a4b84eb4da48a38f4d539587d48cc87fd2006
Christopher <cisensano@yahoo.com>
2016-07-21 16:21:02 -0700

    added info section

--- INCLUDES_CUSTOM/removeAppTransferRelationship.js
+++ INCLUDES_CUSTOM/removeAppTransferRelationship.js
@@ -1,3 +1,14 @@
+/*------------------------------------------------------------------------------------------
+|	File Name : removeAppTransferRelationship.js
+|	Agency : MSP
+|	Events : ASA:MCD/Intrastate Motor Carrier/Application/NA
+|	Notes :
+|			Added to INCLUDES_CUSTOM Replacing Standard Choice lines:
+|				01 - !cap.isCreatedByACA() && getParent() != null ^ pId = getParent() ^ pId = null;
+|				02 - pId ^ pCap = aa.cap.getCap(pId).getOutput(); parentAppType = pCap.getCapType().toString(); logDebug(parentAppType);
+|				03 - pId && parentAppType == "MCD/Intrastate Motor Carrier/Transfer/NA" ^ editAppSpecific("Transfer Application Number", pId.getCustomID());
+|				04 - pId ^ removeParent(pId.getCustomID());
+------------------------------------------------------------------------------------------*/
 function removeAppTransferRelationship(){
 	if(!cap.isCreatedByACA() && getParent() != null){
 		pId = getParent();
