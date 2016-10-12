f9e012f8ac9bc4404ecd2a63c5cb8641a2237f34
Christopher <cisensano@yahoo.com>
2016-10-12 15:35:26 -0700

    Added users

--- Scripts/Misc/INCLUDES_CUSTOM_GLOBALS.js
+++ Scripts/Misc/INCLUDES_CUSTOM_GLOBALS.js
@@ -1,1 +1,1 @@
-if(matches(currentUserID, "ADMIN", "CGODWIN")) showDebug = 3; //user names need to be added in ALL CAPS!
+if(matches(currentUserID, "ADMIN", "CGODWIN", "SAPKOTAT", "HOWER")) showDebug = 3; //user names need to be added in ALL CAPS!
